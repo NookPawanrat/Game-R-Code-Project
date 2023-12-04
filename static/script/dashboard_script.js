d4a618b8632fbbc24a138ca494e5a309ed638077
Zhou Binchi <zbinchi@outlook.com>
2023-12-04 09:36:41 +0200

    Finished dashboard html, css, JS, and created main.py.

---
+++ static/script/dashboard_script.js
@@ -0,0 +1,7 @@
+document.getElementById('showcountries').addEventListener('click', function(){
+  window.open("http://127.0.0.1:5000/countries");
+})
+
+document.getElementById('howtoplay').addEventListener('click', function(){
+  window.location.href = 'http://127.0.0.1:5000/howtoplay';
+})
