dd656a12dfdc6361fe7e9aede2c37bb1bb28ca09
First work <pty997355165@outlook.com>
2023-12-08 22:33:47 +0200

    pop-up commit

---
+++ static/js/popup.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Function to open the pop up
+function openPop() {
+  const modal = document.querySelector('dialog');
+  modal.showModal();
+}
+
+// Function to close the pop up
+function closePop() {
+  const modal = document.querySelector('dialog');
+  modal.close();
+}
